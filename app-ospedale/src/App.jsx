0c1a70c3eabb20157a1509dbde2ca199ce5572cf
Alessandro Rasia dal Polo <ale.rasiadalpolo12@gmail.com>
2024-04-07 22:53:39 +0200

    creati primi componenti, da sistemare la source delle immagini

--- app-ospedale/src/App.jsx
+++ app-ospedale/src/App.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import styles from "./style";
+import { Navbar, Hero, Footer, Testimonials, Clients,Stats } from "./components";
 
-const App = () => {
-  return (
-    <div>
-      
+const App = () =>
+(
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
     </div>
-  )
-}
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Clients />
+        <Testimonials />
+        <Footer />
+      </div>
+    </div>
+
+  </div>
+);
 
 export default App
